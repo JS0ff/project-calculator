fe6e5a2cf20d5af86f87547281d6e4b8a253e6d4
Asan <onlylearningjs@gmail.com>
2025-04-10 22:24:01 +0600

    Create a function to create an equation

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ function divide(a, b) {
 let calcFirstNumber;
 let calcSecondNumber;
 let calcOperator;
+
+//Function that takes users data and gives the result based on operator
+function operation(calcOperator, calcFirstNumber, calcSecondNumber) {
+  switch (calcOperator) {
+    case "+":
+      return add(calcFirstNumber, calcSecondNumber);
+    case "-":
+      return subtract(calcFirstNumber, calcSecondNumber);
+    case "*":
+      return multiply(calcFirstNumber, calcSecondNumber);
+    case "/":
+      return divide(calcFirstNumber, calcSecondNumber);
+  }
+}
