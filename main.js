fa3e1f178c3774ba1938416be6add04b4b7470aa
Asan <onlylearningjs@gmail.com>
2025-04-13 19:20:36 +0600

    Evaluate only two numbers in one time

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ let calcOperator;
 
 //Function that takes users data and gives the result based on operator
 function operate(calcOperator, calcFirstNumber, calcSecondNumber) {
+  calcFirstNumber = Number(calcFirstNumber);
+  calcSecondNumber = Number(calcSecondNumber);
   switch (calcOperator) {
     case "+":
       return add(calcFirstNumber, calcSecondNumber);
@@ -37,15 +39,20 @@ function inputUserNum() {
   calcDisplay.textContent = "";
   for (const element of buttons) {
     element.addEventListener("click", function () {
+      //If there is already value in calcFirstNumber input calcSecondNumber
       if (
         calcOperator &&
         (Number(element.textContent) ||
           element.textContent === "0" ||
           element.textContent === "00")
       ) {
+        if (calcSecondNumber === "") {
+          calcDisplay.textContent = "";
+        }
         calcDisplay.textContent += element.textContent;
         calcSecondNumber = calcDisplay.textContent;
       } else if (
+        // Save the first user number
         Number(element.textContent) ||
         element.textContent === "0" ||
         element.textContent === "00"
@@ -53,6 +60,26 @@ function inputUserNum() {
         calcDisplay.textContent += element.textContent;
         calcFirstNumber = calcDisplay.textContent;
       } else if (
+        calcOperator &&
+        (element.textContent === "+" ||
+          element.textContent === "-" ||
+          element.textContent === "*" ||
+          element.textContent === "/")
+      ) {
+        calcDisplay.textContent = operate(
+          calcOperator,
+          calcFirstNumber,
+          calcSecondNumber
+        );
+        calcFirstNumber = calcDisplay.textContent;
+        calcSecondNumber = "";
+
+        console.log(calcFirstNumber);
+        console.log(calcSecondNumber);
+        // calcFirstNumber = Number(calcDisplay.textContent);
+        calcOperator = element.textContent;
+      } else if (
+        //Save user's operator
         element.textContent === "+" ||
         element.textContent === "-" ||
         element.textContent === "*" ||
@@ -60,10 +87,9 @@ function inputUserNum() {
       ) {
         calcOperator = element.textContent;
         calcDisplay.textContent = "";
-      } else if (element.textContent === "=") {
-        calcFirstNumber = Number(calcFirstNumber);
-        calcSecondNumber = Number(calcSecondNumber);
-        console.log(operate(calcOperator, calcFirstNumber, calcSecondNumber));
+      }
+      // If the user press "=" display the result
+      else if (element.textContent === "=") {
         calcDisplay.textContent = operate(
           calcOperator,
           calcFirstNumber,
