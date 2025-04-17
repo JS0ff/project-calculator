16fbc7db2fe6aab8837641401739648d28c51f38
Asan <onlylearningjs@gmail.com>
2025-04-17 14:36:38 +0600

    Create three functions for every user input

--- main.js
+++ main.js
@@ -58,136 +58,31 @@ function checkAllOperation() {
         (Number(calcFirstNumber) / Number(calcSecondNumber)) * 1000000
       ) /
         1000000
-  ) {
+  )
     return true;
-  } else {
-    false;
-  }
+  else return false;
 }
 
 let calcDisplay = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-
 const dotBtn = document.querySelector("#dot");
 
-function inputUserNum() {
+//Create functions for each action
+//Create main function that will create the result
+function inputFirstNumber() {
   calcDisplay.textContent = "";
   for (const element of buttons) {
     element.addEventListener("click", function () {
-      // Checks for dots in a display number,
-      // if finds one deletes event listener and colors the dot button to the gray
-      dotsAmount = calcDisplay.textContent
-        .split("")
-        .filter((item) => item === ".");
-
-      if (dotsAmount.length === 1)
-        dotBtn.removeEventListener("click", function () {});
-      else if (
-        element.textContent === "." ||
-        (calcFirstNumber && element.textContent === ".")
-      )
-        calcDisplay.textContent += ".";
-
-      // If backspace button clicked, check if which number this is and
-      // check if there is already dot inside the number.
-      if (
-        element.textContent === "␈" &&
-        !calcOperator &&
-        (!checkAllOperation() || !calcSecondNumber)
-      ) {
-        calcDisplay.textContent = calcDisplay.textContent
-          .split("")
-          .slice(0, length - 1)
-          .join("");
-        calcFirstNumber = calcDisplay.textContent;
-      } else if (
-        element.textContent === "␈" &&
-        calcOperator &&
-        !checkAllOperation()
-      ) {
-        calcDisplay.textContent = calcDisplay.textContent
-          .split("")
-          .slice(0, length - 1)
-          .join("");
-        calcSecondNumber = calcDisplay.textContent;
-      }
-
-      //If there is already value in calcFirstNumber input calcSecondNumber
-      else if (
-        //If operator has a value skip inputting data to the first user number
-        // and input the value to the second number
-        calcOperator &&
-        (Number(element.textContent) || element.textContent === "0")
-      ) {
-        // if second user number has empty string then input to the second number
-        if (calcSecondNumber === "") {
-          calcDisplay.textContent = "";
-          calcDisplay.textContent += element.textContent;
-          calcSecondNumber = calcDisplay.textContent;
-        }
-        // If first number and second number sum will be equal to displayed number,
-        // remove all data from display and save the current sum to the first number
-        else if (checkAllOperation()) {
-          console.log("here");
-          clearUserInputData();
-          calcDisplay.textContent += element.textContent;
-          calcFirstNumber = calcDisplay.textContent;
-        } else {
-          //Else just save the value to the second number
-          calcDisplay.textContent += element.textContent;
-          calcSecondNumber = calcDisplay.textContent;
-        }
-      } else if (
-        // Save the first user number
-        Number(element.textContent) ||
-        element.textContent === "0"
-      ) {
+      if (Number(element.textContent) || element.textContent === "0") {
         calcDisplay.textContent += element.textContent;
-        calcFirstNumber = calcDisplay.textContent;
-      } else if (
-        //Alert if the user continues to press operator button
-        calcOperator &&
-        !calcSecondNumber &&
-        operators.includes(element.textContent)
-      ) {
-        alert("Please input the second number");
-      } else if (calcOperator && operators.includes(element.textContent)) {
-        calcDisplay.textContent = operate(
-          calcOperator,
-          calcFirstNumber,
-          calcSecondNumber
-        );
-        calcFirstNumber = calcDisplay.textContent;
-        calcSecondNumber = "";
-        // calcFirstNumber = Number(calcDisplay.textContent);
-        calcOperator = element.textContent;
-      } else if (
-        //Save user's operator
-        operators.includes(element.textContent) &&
-        calcFirstNumber
-      ) {
-        calcOperator = element.textContent;
-        calcDisplay.textContent = "";
+      } else if (operators.includes(element.textContent)) {
+        return calcDisplay.textContent;
       }
-      // If the user press "=" display the result
-      else if (element.textContent === "=") {
-        //Check if the user wants to divide by zero
-        if (calcOperator === "/" && calcSecondNumber === "0") {
-          alert("Error! Do not divide by zero");
-          clearUserInputData();
-        } else {
-          calcDisplay.textContent = operate(
-            calcOperator,
-            calcFirstNumber,
-            calcSecondNumber
-          );
-        }
-      }
-      // Clear Button
-      else if (element.textContent === "C") clearUserInputData();
-      console.log(calcFirstNumber, calcOperator, calcSecondNumber);
     });
   }
 }
+function inputSecondNumber() {}
+function inputOperation() {}
 
-inputUserNum();
+// Main function
+function calculator() {}
